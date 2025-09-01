c6f0f6d43cef596492b2604fc19d7fa24d15926f
Iván H. <ivanprofegh@gmail.com>
2025-09-01 19:36:32 +0000

    Te proporciono dos textos. Cárgalos tú en septiembre.ts
    1 de septiembre

--- src/lib/texts/septiembre.ts
+++ src/lib/texts/septiembre.ts
@@ -3,6 +3,29 @@ import type { DailyText } from '@/lib/texts';
 
 const dailyTexts: DailyText[] = [
   {
+    level: 'secundaria',
+    day: 1,
+    text: `Bienvenidos a este momento de oración, queremos unirnos a nuestros compañeros del colegio. Queremos ponernos en manos de Dios para que nos ilumine y aliente en saber hacer la parte que nos corresponde para hacer posible un mundo mejor, el mundo más justo, fraterno y solidario que soñamos, la CASA COMÚN para todos.
+El lema de este curso dice: “SUEÑA PAZ Y SIEMBRA EL BIEN”
+Momento de reflexión personal, piensa en los sueños de paz que te gustaría se hicieran realidad y que estas dispuesto/a a sembrar (realizar) para conseguirlos.`
+  },
+  {
+    level: 'secundaria',
+    day: 2,
+    text: `ORACIÓN
+Sois semillas del Reino.
+Sois semillas del Reino plantadas en la historia.
+Sois buenas y tiernas, llenas de vida.
+Os tengo en mi mano, os acuno y quiero, y por eso os lanzo al mundo: ¡Perdeos!
+No tengáis miedo a tormentas ni sequías, a pisadas ni espinos.
+Bebed de los pobres, dejaos tocar el alma por ellos y empapaos de mi rocío, de mi presencia.
+Floreced y dad fruto... contribuid a hacer de este mundo un vergel.
+Dejaos mecer por el viento de mi Espíritu.
+Que todo viajero que ande por sendas y caminos, buscando o perdido, caído o herido, al veros, sienta un vuelco al encontrar en vosotros calor humano, fraterno, y pueda sentirse amado, rescatado, levantado, sanado... SALVADO.
+¡Sois semillas de mi Reino! «Antes de formaros en el vientre de vuestra madre, yo os escogí.
+Antes de que salierais del seno materno, os consagré. Como luz del mundo os constituí. No tengáis miedo, que Yo estoy con vosotros» (CfrJr 1,5.8)`
+  },
+  {
     level: 'infantil-primaria',
     day: 10, // Corresponde al 10 de septiembre
     text: '¡Bienvenidos de nuevo al cole! Hoy es un día para reencontrarse con los amigos y empezar con alegría.'
