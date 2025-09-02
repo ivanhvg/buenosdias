1c7201e8c5a1ba07073477dab9ad48f33767c0ee
Iván H. <ivanprofegh@gmail.com>
2025-09-02 16:29:34 +0000

    10 DE SEPTIEMBRE Acogida en el salón de actos. Oración.
    11 DE SEPTIEM

--- src/lib/texts/septiembre.ts
+++ src/lib/texts/septiembre.ts
@@ -41,7 +41,7 @@ Que al final del día, sienta la cercanía de tu presencia y la satisfacción de
   {
     level: 'secundaria',
     day: 10, // Corresponde al 10 de septiembre
-    text: 'El comienzo de un nuevo curso es una oportunidad para marcarse nuevas metas. ¿Qué te gustaría conseguir este año?'
+    text: 'Acogida en el salón de actos. Oración.'
   },
   {
     level: 'primaria',
@@ -56,12 +56,30 @@ Que al final del día, sienta la cercanía de tu presencia y la satisfacción de
   {
     level: 'secundaria',
     day: 11,
-    text: 'La sostenibilidad no es solo reciclar. Implica pensar en cómo nuestras acciones de hoy afectarán al mundo de mañana. ¿De qué manera tus hábitos de consumo impactan en el medio ambiente y qué podrías cambiar?'
+    text: `Bienvenidos a este momento de oración, queremos unirnos a nuestros compañeros del colegio. Queremos ponernos en manos de Dios para que nos ilumine y aliente en saber hacer la parte que nos corresponde para hacer posible un mundo mejor, el mundo más justo, fraterno y solidario que soñamos, la CASA COMÚN para todos.
+ El lema de este curso dice:
+
+“SUEÑA PAZ Y SIEMBRA EL BIEN”
+
+Momento de reflexión personal,  piensa en los sueños de paz que te gustaría se hicieran realidad y que estas dispuesto/a a sembrar (realizar) para conseguirlos.`
   },
   {
     level: 'secundaria',
     day: 12,
-    text: 'La solidaridad va más allá de dar lo que nos sobra. Significa empatizar con las luchas de otros y actuar. ¿Qué problema social te preocupa y cómo podrías involucrarte para generar un cambio positivo?'
+    text: `ORACIÓN
+
+Sois semillas del Reino.
+Sois semillas del Reino plantadas en la historia. 
+Sois buenas y tiernas, llenas de vida. 
+Os tengo en mi mano, os acuno y quiero, y por eso os lanzo al mundo: ¡Perdeos! 
+No tengáis miedo a tormentas ni sequías, a pisadas ni espinos. 
+Bebed de los pobres, dejaos tocar el alma por ellos y empapaos de mi rocío, de mi presencia. 
+Floreced y dad fruto… contribuid a hacer de este mundo un vergel. 
+Dejaos mecer por el viento de mi Espíritu. 
+Que todo viajero que ande por sendas y caminos, buscando o perdido, caído o herido, al veros, sienta un vuelco al encontrar en vosotros calor humano, fraterno, y pueda sentirse amado, rescatado, levantado, sanado… SALVADO. 
+¡Sois semillas de mi Reino! «Antes de formaros en el vientre de vuestra madre, yo os escogí. 
+Antes de que salierais del seno materno, os consagré. Como luz del mundo os constituí. 
+No tengáis miedo, que Yo estoy con vosotros» (CfrJr 1,5.8)`
   }
 ];
 
