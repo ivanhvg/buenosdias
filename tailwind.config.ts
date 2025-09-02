118be3ecb87c98d2ae9846158e539a0fc55637ba
Iván H. <ivanprofegh@gmail.com>
2025-09-02 22:04:09 +0000

    El texto de cabecera ¡Buenos días! cámbialo a una tipografía handwriting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Literata', 'serif'],
-        headline: ['Literata', 'serif'],
+        headline: ['"Patrick Hand"', 'cursive'],
         code: ['monospace'],
       },
       colors: {
