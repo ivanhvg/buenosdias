8078a80d9f50c93c9696d73de3bb6dc23a9f5049
Iván H. <ivanprofegh@gmail.com>
2025-09-02 15:47:24 +0000

    Quiero actualizar los textos de septiembre, pero aquí en Firebase hay un

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,10 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-require('dotenv').config();
 
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
+      apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
     }),
   ],
 });
