ffe191fc7a6c8534e5e4dd1016c25313c071f95c
Iván H. <ivanprofegh@gmail.com>
2025-09-02 20:53:55 +0000

    Insértalo en día 2 para verlo ahora.

--- src/lib/texts/septiembre.ts
+++ src/lib/texts/septiembre.ts
@@ -6,7 +6,7 @@ import type { DailyText } from '@/lib/texts';
 const dailyTexts: DailyText[] = [
   {
     level: 'secundaria',
-    day: 24,
+    day: 2,
     text: 'ORACIÓN\nDios de toda la vida, te damos gracias por el don de la tierra, nuestra casa común. En este momento, muchas personas, los océanos y los ecosistemas de la tierra están luchando por sobrevivir. Ayúdanos a cambiar nuestra forma de actuar y guíanos a vivir una vida que no se centre exclusivamente en nosotros mismos, sino en las necesidades de los demás y en el culto a ti como nuestra máxima preocupación. Escúchanos, Señor.\nOremos unos por otros en este momento para que podamos vivir la visión y los valores de la encíclica del Papa Francisco sobre el cuidado de nuestra casa común. En particular, pedimos que se preste mayor atención a la justicia social y la justicia climática. Escúchanos, Señor.\nSeñor, te pedimos que podamos apreciar más profundamente la belleza y la diversidad de todo lo que has creado. Mientras recorremos nuestro curso, ayúdanos a escuchar también la voz de la creación, a reconocer lo profundamente interconectados que estamos entre nosotros y con el mundo natural. Escúchanos, Señor.\nSeñor, en el espíritu de la llamada de tu Hijo, Jesús, a seguirle, renunciando a nosotros mismos, que nos decidamos a frenar todas las acciones individuales egoístas que entran en conflicto con nuestra interrelación entre nosotros y con el mundo natural. Te pedimos que nos apartemos de los combustibles fósiles y elijamos nuevas opciones de inversión, como las energías renovables, y que compartamos mejor los dones de la creación. Escúchanos, Señor.'
   }
 ];
