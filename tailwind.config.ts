f58ab11dca7a2e0929b3d9b35efc37468c7a8d59
Iván H. <ivanprofegh@gmail.com>
2025-09-02 22:08:04 +0000

    Prueba la fuente "Dancing Script" de Google Fonts Designed by Impallari

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Literata', 'serif'],
-        headline: ['"Patrick Hand"', 'cursive'],
+        headline: ['"Dancing Script"', 'cursive'],
         code: ['monospace'],
       },
       colors: {
