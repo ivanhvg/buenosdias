219e4e808aa01e12ddf6f17d81a38d87afbf8d77
Iván H. <ivanprofegh@gmail.com>
2025-09-05 16:41:19 +0000

    Cuando envío la url del la web por Whatsapp, el mensaje que describe el

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,11 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        <meta property="og:title" content="Buenos días · BCLL" />
+        <meta property="og:description" content="Nuestro espacio diario para la reflexión y la oración grupal." />
+        <meta property="og:image" content="https://www.buenconsejolalaguna.com/wp-content/uploads/2022/03/logoBCLL.png" />
+        <meta property="og:url" content="https://buenos-dias-bcll.web.app" />
+        <meta property="og:type" content="website" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Literata:opsz,wght@24..96,400;24..96,700&display=swap" rel="stylesheet" />
